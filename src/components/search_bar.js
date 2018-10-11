1839d997d57f9ef6112d8b1b40872a9875f66a94
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2018-10-11 16:19:37 -0500

    worked on search bar a little.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Z_ASCII } from 'zlib';
 
 
 class SearchBar extends Component { 
+    constructor (props) {
+        super(props);
+
+        this.state = {term: ''};
+    }
     render() {
-        return <input />;
+        return (
+            <div>
+                <input 
+                value= {this.state.term}
+                onChange = {event => this.setState({ term: event.target.value})} />
+            </div>
+        );
     }
 }
 
